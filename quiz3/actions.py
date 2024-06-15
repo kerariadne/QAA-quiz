bd80e441c25df43ec65f4117bf13d212b8d888e7
Tamar Abramishvili <tamarabramishvili2@gmail.com>
2024-06-16 00:59:35 +0400

    edit

--- quiz3/actions.py
+++ quiz3/actions.py
@@ -1,12 +1,23 @@
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators import LoginPageLocators
-
+import time
 class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
     def login(self, username, password):
         self.driver.find_element(*LoginPageLocators.USERNAME_INPUT).send_keys(username)
         self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
         self.driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
 
+
+    def get_password(self):
+        print("Get Password:")
+        return self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT).get_attribute("value")
+    
     def get_error_message(self):
         return self.driver.find_element(*LoginPageLocators.ERROR_MESSAGE).text
 
@@ -29,12 +40,6 @@ class LoginPage:
     def go_to_cart(self):
         self.driver.find_element(*LoginPageLocators.CART_ICON).click()
 
-    def get_product_price(self):
-        return self.driver.find_element(*LoginPageLocators.PRODUCT_PRICE).text
-
-    def get_product_name(self):
-        return self.driver.find_element(*LoginPageLocators.PRODUCT_NAME).text
-
     def go_to_checkout(self):
         self.driver.find_element(*LoginPageLocators.CHECKOUT_BUTTON).click()
 
@@ -43,6 +48,3 @@ class LoginPage:
         self.driver.find_element(*LoginPageLocators.LAST_NAME_INPUT).send_keys(last_name)
         self.driver.find_element(*LoginPageLocators.POSTAL_CODE_INPUT).send_keys(postal_code)
         self.driver.find_element(*LoginPageLocators.CONTINUE_BUTTON).click()
-
-    def get_error_message(self):
-        return self.driver.find_element(*LoginPageLocators.ERROR_MESSAGE).text
--- quiz3/locators.py
+++ quiz3/locators.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.common.by import By
 
-class Locators:
+class LoginPageLocators:
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
     SORT_SELECT = (By.CLASS_NAME, "product_sort_container")
-    ADD_TO_CART_BUTTON = (By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/button")
-    REMOVE_BUTTON = (By.XPATH, '//*[@id="remove-sauce-labs-backpack"]')
+    ADD_TO_CART_BUTTON = (By.ID, "add-to-cart-sauce-labs-onesie")
+    REMOVE_BUTTON = (By.ID, 'remove-sauce-labs-onesie')
     CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
     PRODUCT_PRICE = (By.CLASS_NAME, "inventory_item_price")
     PRODUCT_NAME = (By.CLASS_NAME, "inventory_item_name")
